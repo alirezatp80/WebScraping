c1268026af10654d184b10954b9be2231e30d1ed
AlirezaTashani <alirezatd80@gmail.com>
2026-02-24 05:01:05 +0330

    final initialize book class

--- Books/main.py
+++ Books/main.py
@@ -6,6 +6,16 @@ class Book:
         self.year = year
         self.page_number = page_number
         self.description = description
+class BookRepository:
+    def __init__(self ):
+        self.books = []
+    def add_book(self):
+        pass
+    def remove_book(self):
+        pass
+    def scrap_or_read(self):
+        pass
+        
 
 class History_book(Book):
     
@@ -18,3 +28,19 @@ class Tech_book(Book):
         super().__init__(name, photo_url, author, year, page_number, description)
         self.url = url
         
+class Scraper:
+    def __init__(self , url):
+        self.url = url
+        
+    def scrap(self):
+        pass
+    def parse(self):
+        pass
+    
+class Database:
+    def __init__(self , filename):
+        self.filename = filename
+    def save(self):
+        pass
+    def read(self):
+        pass
