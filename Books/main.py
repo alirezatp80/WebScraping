a4e6de7f6b92d38691800a609b3b5a9f905b0302
AlirezaTashani <alirezatd80@gmail.com>
2026-02-25 16:08:33 +0330

    complate

--- Books/main.py
+++ Books/main.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import asyncio
 import aiohttp
-import re
-
+import json
 def change_html_to_json(soup):
     result = dict()
     for index , tr in enumerate(soup):
@@ -20,16 +19,30 @@ class Book:
         self.year = year
         self.page_number = page_number
         self.description = description
+    def __eq__(self, book:Book):
+        if self.name == book.name:
+            return True
+    def to_dic(self):
+        return {
+            "name":self.name,
+            "photo_url":self.photo_url,
+            "author":self.author,
+            "year":self.year,
+            "page_number":self.page_number,
+            "description":self.description
+            
+        }
+    def __str__(self):
+        return self.to_dic()
 
 class BookRepository:
     def __init__(self ):
         self.books = []
-    def add_book(self):
-        pass
-    def remove_book(self):
-        pass
-    def scrap_or_read(self):
-        pass
+    def add_book(self , book:Book):
+        if book not in self.books:
+            self.books.append(book)
+    
+
         
 
 class History_book(Book):
@@ -85,7 +98,7 @@ class Scraper:
             photo_link = soup.select_one('a.book-cover').get('href')
             author = soup.select_one('div.book-main-info-authors ul li a').text
             dic_info = (change_html_to_json((table)))
-           
+            print('proccessing...')
             return {
                 'name':dic_info['نام کتاب'].strip(),
                 'author':author,
@@ -106,17 +119,18 @@ class Scraper:
     sem = asyncio.Semaphore(40)
     
 class Database:
-    def __init__(self , filename):
+    def __init__(self , filename , repo:BookRepository):
         self.filename = filename
+        self.repo = repo
     def save(self):
-        pass
+        with open(f'{self.filename}.json' , 'w' , encoding='utf-8')as f:
+            json.dump(self.repo , f , ensure_ascii=False , indent=4)
     def read(self):
-        pass
+        with open(f'{self.filename}.json' , 'r' , encoding='utf-8' ) as f:
+          return  json.load(f)
     
     
 
 
-
-    
 
     
